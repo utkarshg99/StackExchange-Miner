aed38d63ad57338e0dd23275e04558e0d4ede9fc
Utkarsh Gupta <utkarshg99@gmail.com>
2021-11-02 08:57:51 +0530

    automated finding and downloading

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+const https = require("https");
 const fs = require("fs");
 const cheerio = require("cheerio");
 const axios = require("axios");
-const pretty = require("pretty");
-const { data } = require("cheerio/lib/api/attributes");
+const Seven = require("node-7z");
+const Downloader = require('nodejs-file-downloader');
 
-const mainURL = "https://archive.org/download/stackexchange";
+const mainURL = "https://archive.org/download/stackexchange/";
 const subBaseURL = "http://ia600107.us.archive.org/view_archive.php?archive=/27/items/stackexchange/";
+const FILE_DOWNLOAD_PATH = "Data/";
+const FILE_EXTRACT_PATH = "Data/Extracted/";
+const SCRAPE_OUTPUT_PATH = "Scraping/";
+const SCRAPE_CSV = "xmls.csv";
+const VERIFY_CSV = "xmls-v.csv";
+const SCRAPE_JSON = "xmls.json";
+const SCRAPE_TSV = "main.tsv";
+const FILES = ["Badges.xml", "Comments.xml", "PostHistory.xml", "PostLinks.xml", "Posts.xml", "Tags.xml", "Users.xml", "Votes.xml"];
 
 function processMain(){
-    let raw_txt = String(fs.readFileSync("Scraping/main.tsv"));
+    let raw_txt = String(fs.readFileSync(SCRAPE_OUTPUT_PATH + SCRAPE_TSV));
     raw_txt = raw_txt.split("\n");
     let arr = [];
     let szs = [];
@@ -26,6 +35,7 @@ async function scrapeAll() {
     let meta_indx = meta["array"];
     let sizes = meta["sizes"];
     let lines = ["StackExchange,Size,Number of XMLs"];
+    let scrape = {}
     for (let i = 0; i < meta_indx.length; i++) {
         if (!meta_indx[i].length) continue;
         let resp = await axios.get(subBaseURL + meta_indx[i]);
@@ -36,7 +46,7 @@ async function scrapeAll() {
             "size": sizes[i],
             "xmls": []
         };
-        let x_line = `${meta_indx[i]},${sizes[i]},${table_ele.find("a").length}`;
+        let x_line = `${meta_indx[i]},${sizes[i]},${table_ele.find("a").length-1}`;
         table_ele.find("a").each((index, element) => {
             if($(element).text().length) {
                 data.xmls.push({
@@ -46,10 +56,87 @@ async function scrapeAll() {
                 x_line += ","+$(element).text();
             }
         });
+        scrape[meta_indx[i]] = data;
         lines.push(x_line);
-        if(lines.length % 10 == 0) fs.writeFileSync("Scraping/xmls.csv", lines.join("\n"));
+        if(lines.length % 10 == 0) fs.writeFileSync(SCRAPE_OUTPUT_PATH + VERIFY_CSV, lines.join("\n"));
     }
-    fs.writeFileSync("Scraping/xmls.csv", lines.join("\n"));
+    fs.writeFileSync(SCRAPE_OUTPUT_PATH + VERIFY_CSV, lines.join("\n"));
+    return scrape;
 }
 
-fs.writeFileSync("Scraping/xmls.json", JSON.stringify(scrapeAll()));
+async function verifyAll() {
+    let meta = processMain();
+    let meta_indx = meta["array"];
+    let sizes = meta["sizes"];
+    let lines = ["StackExchange,Size,Number of XMLs"];
+    for (let i = 0; i < meta_indx.length; i++) {
+        let fpresent = [];
+        if (!meta_indx[i].length) continue;
+        let x_line = `${meta_indx[i]},${sizes[i]}`;
+        for (let j = 0; j < FILES.length; j++) {
+            try {
+                let resp = await axios.head(mainURL + meta_indx[i] + "/" + FILES[j]);
+                if (resp.status / 100 == 2) fpresent.push(FILES[j]);
+            }
+            catch(e) {console.log(meta_indx[i] + "/" + FILES[j]);}
+        }
+        x_line += ","+fpresent.length;
+        for (let j = 0; j < fpresent.length; j++) x_line += ","+fpresent[j];
+        lines.push(x_line);
+        fs.writeFileSync(SCRAPE_OUTPUT_PATH + SCRAPE_CSV, lines.join("\n"));
+    }
+    fs.writeFileSync(SCRAPE_OUTPUT_PATH + SCRAPE_CSV, lines.join("\n"));
+    return meta;
+}
+
+async function downloadFile(urls) {
+    let fnames = [];
+    for (let i = 0; i < urls.length; i++){
+        let url = urls[i];
+        let durl = mainURL + url;
+        let fname = url;
+        let downloader = new Downloader({
+            url: durl,
+            directory: FILE_DOWNLOAD_PATH,
+            cloneFiles: false,
+            maxAttempts: 3
+        });
+        await downloader.download();
+        console.log(fname);
+        fnames.push(fname);
+    }
+    return fnames;
+}
+
+async function unzipFiles(fnames) {
+    for (let i = 0; i < fnames.length; i++) {
+        let name = fnames[i]; 
+        let out_f = FILE_EXTRACT_PATH + name.replace(".7z", "");
+        if(!fs.existsSync(out_f)) fs.mkdirSync(out_f);
+        let unz_prom = new Promise((resolve, reject) => {
+            let unz_stream = Seven.extract(FILE_DOWNLOAD_PATH + name, out_f, {
+                recursive: true,
+                $cherryPick: '*.xml'
+            });
+            unz_stream.on('end', () => resolve());
+            unz_stream.on('error', (error) => reject(error));
+        });
+        await unz_prom;
+        console.log(out_f);
+    }
+}
+
+async function findData() {
+    // let _meta = await scrapeAll(); // Generates the xml record as indicated by the website
+    let _meta = await verifyAll(); // Generates the xml record fround by crawling the website
+    fs.writeFileSync(SCRAPE_OUTPUT_PATH + SCRAPE_JSON, JSON.stringify(_meta));
+}
+
+async function getData() {
+    let urls = ["chinese.stackexchange.com.7z", "emacs.stackexchange.com.7z", "history.stackexchange.com.7z"]; // The stackexchange sub-domains to download data from
+    let fnames = await downloadFile(urls);
+    await unzipFiles(fnames); // unzip the downloaded tars
+}
+
+// findData();
+getData();
---
+++ start.sh
@@ -0,0 +1,6 @@
+npm install
+mkdir -p Data
+mkdir -p Scraping
+mkdir -p Data/Extracted
+
+node index.js > logs.dump
