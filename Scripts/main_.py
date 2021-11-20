85ea54572378241ab66b2b683b910b3d22976d6e
Ashwin Shenai <ashwins@iitk.ac.in>
2021-11-20 20:50:04 +0530

    Add active users script to runner

--- Scripts/main_.py
+++ Scripts/main_.py
@@ -6,3 +6,4 @@ if __name__ == "__main__":
     os.system(f"python3 map_reduce.py {sys.argv[1]}")
     os.system(f"python3 question_time.py {sys.argv[1]}")
     os.system(f"python3 voting_reputation.py {sys.argv[1]}")
+    os.system(f"python3 active_users.py {sys.argv[1]}")
